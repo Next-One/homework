54a3884085b654318eb1df2a3f70da48fd3d0bda
Next-One <1056935467@qq.com>
2021-09-10 22:14:57 +0800

    第二次作业提交

--- src/week02/server.js
+++ src/week02/server.js
@@ -1,4 +1,21 @@
 const http = require('http')
+const {open} = require('fs/promises')
+
+let htmlData = null
+
+
+
+async function getIndexHtmlData(file = './src/week02/index.html') {
+	// if(htmlData) return htmlData 不缓存index数据，每次拿最新的
+	let fileHandler
+	try{
+		fileHandler = await open(file, 'r')
+		const data = await fileHandler.read()
+		return htmlData = data.buffer
+	}finally {
+		if(fileHandler) await fileHandler.close()
+	}
+}
 
 const server = http.createServer((req, res) => {
 	let body = [];
@@ -6,10 +23,11 @@ const server = http.createServer((req, res) => {
 		console.error(err);
 	}).on('data', (chunk) => {
 		body.push(chunk);
-	}).on('end', () => {
+	}).on('end', async () => {
 		body = Buffer.concat(body).toString();
 		res.writeHead(200, {'Content-Type': 'text/html'})
-		res.end('<h1>汪明喜的server</h1>')
+		const buffer = await getIndexHtmlData()
+		res.end(buffer)
 	});
 })
 
